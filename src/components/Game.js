b6795d53182089c893977124d4e22e5746251b9c
Arief R <arief0303@gmail.com>
2023-06-08 11:34:34 +0700

    animation v1

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,12 @@
 import { ref, onMounted, onUnmounted } from 'vue'
 import { useEventListener } from '@vueuse/core'
 import * as BABYLON from 'babylonjs'
-import { CharacterController } from "babylonjs-charactercontroller";
+// import { CharacterController } from "babylonjs-charactercontroller";
+import CharacterController from './CharacterController'
 import * as Colyseus from "colyseus.js";
 import Stats from "stats-js";
 import "babylonjs-loaders";
 import Player from './Player';
-import CharacterCustomization from './CharacterCustomization';
-import { setupCharacterPack } from './character_customize';
 import JoystickController from "joystick-controller";
 
 var statsFPS = new Stats();
@@ -57,22 +56,10 @@ export default {
             function loadPlayer(scene, engine, canvas) {
                 BABYLON.SceneLoader.ImportMesh("", "assets/player/", "ybot.glb", scene, (meshes, particleSystems, skeletons, animationGroups) => {
                     var player = meshes[0];
-                    /* player.scaling = new BABYLON.Vector3(0.1, 0.1, 0.1);
-                    player.position = new BABYLON.Vector3(0, 0, 0);
-                    player.checkCollisions = true;
-                    player.ellipsoid = new BABYLON.Vector3(0.5, 1, 0.5);
-                    player.ellipsoidOffset = new BABYLON.Vector3(0, 1, 0);
                     player.rotation = player.rotationQuaternion.toEulerAngles();
                     player.rotationQuaternion = null;
-                    var skeleton = skeletons[0];
-                    player.skeleton = skeleton;
-                    skeleton.enableBlending(0.1);
-                    let characterCustomization = CharacterCustomization.getInstance();
-                    characterCustomization.setCurrentAvatarPack("Female", "HeadFemaleBase", "BodyFemaleBase", "PantsFemaleBase");
-                    let characterData = characterCustomization.getCurrentAvatarPack();
-                    setupCharacterPack(meshes, characterData); */
-                    player.rotation = player.rotationQuaternion.toEulerAngles();
-                    player.rotationQuaternion = null;
+                    // set the blending speed for the group
+                    animationGroups.blendingSpeed = 0.01; // adjust this value to control the blending speed
                     const myAgmap = {
                         "idle": animationGroups[0],
                         "walk": animationGroups[6],
@@ -107,37 +94,39 @@ export default {
                     // camera.attachControl(canvas, false);
 
                     //var CharacterController = org.ssatguru.babylonjs.component.CharacterController;
-                    var cc = new CharacterController(player, camera, scene, myAgmap);
-                    cc.setTurningOff(false);
+                    var cc = new CharacterController(player, camera, scene);
+                    cc.setupConfig(myAgmap);
+                    cc.enableBlending();
+                    // cc.setTurningOff(false);
                     //below makes the controller point the camera at the player head which is approx
                     //1.5m above the player origin
-                    cc.setCameraTarget(new BABYLON.Vector3(0, 1.5, 0));
-                    cc.setCameraElasticity(false);
+                    // cc.setCameraTarget(new BABYLON.Vector3(0, 1.5, 0));
+                    // cc.setCameraElasticity(false);
                     //if the camera comes close to the player we want to enter first person mode.
-                    cc.setNoFirstPerson(false);
+                    // cc.setNoFirstPerson(true);
                     //the height of steps which the player can climb
-                    cc.setStepOffset(0.4);
+                    // cc.setStepOffset(0.4);
                     //the minimum and maximum slope the player can go up
                     //between the two the player will start sliding down if it stops
-                    cc.setSlopeLimit(30, 60);
+                    // cc.setSlopeLimit(30, 60);
 
                     //tell controller 
                     // - which animation range should be used for which player animation
                     // - rate at which to play that animation range
                     // - wether the animation range should be looped
                     //use this if name, rate or looping is different from default
-                    cc.setIdleAnim("idle", 1, true);
-                    cc.setTurnLeftAnim("turnLeft", 0.5, true);
-                    cc.setTurnRightAnim("turnRight", 0.5, true);
-                    cc.setWalkBackAnim("walkBack", 0.5, true);
-                    cc.setIdleJumpAnim("idleJump", 0.6, false);
-                    cc.setRunJumpAnim("runJump", 0.6, false);
+                    // cc.setIdleAnim("idle", 1, true);
+                    // cc.setTurnLeftAnim("turnLeft", 0.5, true);
+                    // cc.setTurnRightAnim("turnRight", 0.5, true);
+                    // cc.setWalkBackAnim("walkBack", 0.5, true);
+                    // cc.setIdleJumpAnim("idleJump", 0.6, false);
+                    // cc.setRunJumpAnim("runJump", 0.6, false);
                     //set the animation range name to "null" to prevent the controller from playing
                     //a player animation.
                     //here even though we have an animation range called "fall" we donot want to play 
                     //the fall animation
-                    cc.setFallAnim("fall", 2, false);
-                    cc.setSlideBackAnim("slideBack", 1, false)
+                    // cc.setFallAnim("fall", 2, false);
+                    // cc.setSlideBackAnim("slideBack", 1, false)
                     animationGroups[0].stop(); //stop default animation from playing overlapping with idle anim
                     cc.start();
 
